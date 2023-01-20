ffdb414635a08e8ad62719f869959ff315f3491d
kosmilo <115208829+kosmilo@users.noreply.github.com>
2023-01-20 18:29:34 +0200

    Running and stamina system
    Lisättiin pelaajalle juoksu ja tehtiin sitä rajoittava stamina-systeemi.

--- LaboratoryEscapeGame/Assets/Scripts/PlayerMovement.cs
+++ LaboratoryEscapeGame/Assets/Scripts/PlayerMovement.cs
@@ -6,21 +6,30 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
     GameObject cameraObj;
-    float horizontalInput, verticalInput, mouseXInput, mouseYInput, cameraXAxisClamp;
-    Vector3 cameraRotation, playerRotation, velocity; // camera rotation = up-down, player rotation = left-right
+
+    float horizontalInput, verticalInput, mouseXInput, mouseYInput, runInput;
+    Vector3 cameraRotation, playerRotation, playerMovement; // camera rotation = up-down, player rotation = left-right
+    float cameraXAxisClamp;
+    float rotationSensitivity = 2f;
+
+    float movementSpeed;
+    bool staminaRegenState;
     float walkSpeed = 3f;
     float runSpeed = 6f;
-    float rotationSensitivity = 2f;
+    float maxStamina = 200f;
+    float stamina;
+    float staminaDrain = 1f;
+    float staminaRegen = 0.2f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cameraObj = transform.GetChild(0).gameObject;
+        stamina = maxStamina;
 
-        // Lock and hide the cursor while game is playing
+        // Lock and hide the cursor while the game is playing
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
     }
 
     void Update()
@@ -30,24 +39,23 @@ public class PlayerMovement : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         mouseXInput = Input.GetAxis("Mouse X");
         mouseYInput = Input.GetAxis("Mouse Y");
+        runInput = Input.GetAxis("Run");
 
         HandleRotation();
     }
 
     private void FixedUpdate()
     {
-        // Get the direction player is moving towards based on player input and the direction player is currently facing and multiply it with movement speed
-        velocity = Vector3.ClampMagnitude((transform.forward * verticalInput) + (transform.right * horizontalInput), 1) * walkSpeed;
-        velocity.y = rb.velocity.y;
-
-        rb.velocity = velocity;
+        HandleMovement();
     }
 
     void HandleRotation()
     {
+        // Get the current rotation
         cameraRotation = cameraObj.transform.rotation.eulerAngles;
         playerRotation = transform.rotation.eulerAngles;
 
+        // Change rotation based on inputs
         cameraRotation.x -= mouseYInput * rotationSensitivity;
         cameraRotation.z = 0;
         playerRotation.y += mouseXInput * rotationSensitivity;
@@ -67,5 +75,40 @@ public class PlayerMovement : MonoBehaviour
 
         cameraObj.transform.rotation = Quaternion.Euler(cameraRotation);
         transform.rotation = Quaternion.Euler(playerRotation);
+    }
+
+    void HandleMovement()
+    {
+        // Check if player should be running and assign movement speed accordingly
+        if(runInput > 0.5 && !staminaRegenState)
+        {
+            stamina -= staminaDrain;
+            movementSpeed = runSpeed;
+        }
+        else
+        {
+            stamina += staminaRegen;
+            movementSpeed = walkSpeed;
+        }
+
+        // Set the player's stamina regen state which stops the player from running after stamina reaches 0
+        if(staminaRegenState && stamina > 50)
+        {
+            staminaRegenState = false;
+            Debug.Log("Player movement stamina regen state: " + staminaRegenState);
+        }
+        else if(stamina < 1)
+        {
+            staminaRegenState = true;
+            Debug.Log("Player movement stamina regen state: " + staminaRegenState);
+        }
+        
+
+        // Get the direction player is moving towards based on player input and the direction player is currently facing
+        // and multiply it with movement speed
+        playerMovement = Vector3.ClampMagnitude((transform.forward * verticalInput) + (transform.right * horizontalInput), 1) * movementSpeed;
+        playerMovement.y = rb.velocity.y;
+
+        rb.velocity = playerMovement;
     }
 }
