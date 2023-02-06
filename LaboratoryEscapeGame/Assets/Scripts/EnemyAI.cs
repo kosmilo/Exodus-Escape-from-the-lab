ce6fd9071d9765078c9dfaea950eaf815d865123
kosmilo <115208829+kosmilo@users.noreply.github.com>
2023-02-06 08:59:40 +0200

    Enemy stun and slow effect logic
    Logic needed for stun and slow effect.

--- LaboratoryEscapeGame/Assets/Scripts/EnemyAI.cs
+++ LaboratoryEscapeGame/Assets/Scripts/EnemyAI.cs
@@ -19,7 +19,7 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] Transform playerTransform;
     [SerializeField] LayerMask whatIsGround, whatIsPlayer;
     [SerializeField] float sightRange, attackRange;
-    bool playerInSightRange, playerInAttackRange;
+    bool playerInSight, playerInAttackRange, isStunned;
 
     // Navigating
     [SerializeField] Vector3 walkPoint;
@@ -30,8 +30,9 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] float timeBetweenAttacks;
     bool alreadyAttacked;
 
-    [SerializeField] float roamSpeed;
-    [SerializeField] float chaseSpeed;
+    [SerializeField] float roamSpeed, chaseSpeed;
+    float chaseTime = 4f;
+    float speedMultiplier = 1;
     Vector3 lookOffset = new Vector3(0, 0.5f, 0);
 
 
@@ -60,56 +61,68 @@ public class EnemyAI : MonoBehaviour
                 AttackPlayer();
                 break;
             case EnemyStates.Stunned:
+                Debug.Log(gameObject.name + " stunned");
                 Stunned();
                 break;
         }
     }
 
+    // SETTING THE ENEMY STATE
+
     void SetEnemyState()
     {
-        if (Physics.CheckSphere(transform.position, sightRange, whatIsPlayer))
+        if (isStunned)
         {
-            Ray ray_1 = new Ray(transform.position, playerTransform.position - transform.position);
-            Physics.Raycast(ray_1, out RaycastHit hit_1);
-            Ray ray_2 = new Ray(transform.position + lookOffset, playerTransform.position + lookOffset - transform.position);
-            Physics.Raycast(ray_2, out RaycastHit hit_2);
-            playerInSightRange = hit_1.collider.gameObject.CompareTag("Player") || hit_2.collider.gameObject.CompareTag("Player");
-        }
-        else { playerInSightRange = false; }
-
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-
-        // Set state
-        if (!playerInSightRange)
-        {
-            if (currentState == EnemyStates.Chase || currentState == EnemyStates.Attack)
-            {
-                Invoke("SetStateToRoam", 2f);
-            }
-            else
-            {
-                SetStateToRoam();
-            }
+            currentState = EnemyStates.Stunned;
         }
         else
         {
-            if (!playerInAttackRange)
+            if (Physics.CheckSphere(transform.position, sightRange, whatIsPlayer))
             {
-                currentState = EnemyStates.Chase;
+                Ray ray_1 = new Ray(transform.position, playerTransform.position - transform.position);
+                Physics.Raycast(ray_1, out RaycastHit hit_1);
+                Ray ray_2 = new Ray(transform.position + lookOffset, playerTransform.position + lookOffset - transform.position);
+                Physics.Raycast(ray_2, out RaycastHit hit_2);
+                playerInSight = hit_1.collider.gameObject.CompareTag("Player") || hit_2.collider.gameObject.CompareTag("Player");
+            }
+            else { playerInSight = false; }
+
+            playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+            // Set state
+            if (!playerInSight)
+            {
+                if (currentState == EnemyStates.Chase || currentState == EnemyStates.Attack)
+                {
+                    Invoke("SetStateToRoam", chaseTime);
+                }
+                else
+                {
+                    currentState = EnemyStates.Roam;
+                }
             }
             else
             {
-                currentState = EnemyStates.Attack;
+                if (!playerInAttackRange)
+                {
+                    currentState = EnemyStates.Chase;
+                }
+                else
+                {
+                    currentState = EnemyStates.Attack;
+                }
+                CancelInvoke("SetStateToRoam");
             }
-            CancelInvoke("SetStateToRoam");
         }
     }
 
     void SetStateToRoam() { currentState = EnemyStates.Roam; }
 
+    // ROAMING STATE
+
     void Roaming()
     {
-        agent.speed = roamSpeed;
+        agent.speed = roamSpeed * speedMultiplier;
         if (!walkPointSet)
         {
             SearchWalkPoint();
@@ -140,11 +153,15 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
+    // CHASE STATE
+
     void ChasePlayer()
     {
-        agent.speed = chaseSpeed;
+        agent.speed = chaseSpeed * speedMultiplier;
         agent.SetDestination(playerTransform.position);
     }
+
+    // ATTACK STATE
 
     void AttackPlayer()
     {
@@ -167,17 +184,46 @@ public class EnemyAI : MonoBehaviour
         alreadyAttacked = false;
     }
 
+    // STUNNED STATE
+
     void Stunned()
     {
-        // STUNNED STATE
+        agent.SetDestination(transform.position);
     }
+
+    public void stunEnemy(float stunTime)
+    {
+        isStunned = true;
+        CancelInvoke("ResetStun");
+        Invoke("ResetStun", stunTime);
+    }
+
+    void ResetStun()
+    {
+        isStunned = false;
+    }
+
+    // SLOW EFFECT
+
+    public void slowEnemy(float newSpeedMultiplier, float slowTime)
+    {
+        speedMultiplier = newSpeedMultiplier;
+        CancelInvoke("ResetSpeed");
+        Invoke("ResetSpeed", slowTime);
+    }
+
+    void ResetSpeed()
+    {
+        speedMultiplier = 1f;
+    }
+    
 
     // Draw ranges on editor
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
-        if (playerInSightRange)
+        if (playerInSight)
         {
             Gizmos.DrawRay(transform.position, playerTransform.position - transform.position);
             Gizmos.DrawRay(transform.position + lookOffset, playerTransform.position + lookOffset - transform.position);
