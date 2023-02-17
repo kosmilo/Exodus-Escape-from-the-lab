27585786e11c6ff1d151880bc068b69c7bbf695d
kosmilo <115208829+kosmilo@users.noreply.github.com>
2023-02-17 09:25:29 +0200

    Suffle and check for completion
    Suffle blocks when game starts. Check for completion when moving a piece to the right place. Changed the movement of the pieces.

--- LaboratoryEscapeGame/Assets/Scripts/SlidingBlock.cs
+++ LaboratoryEscapeGame/Assets/Scripts/SlidingBlock.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class SlidingBlock : MonoBehaviour
 {
     [SerializeField] GameObject emptySpace;
+    Vector3 correctPosition;
+    float moveSpeed = 2f;
 
-    void Start()
+    void Awake()
     {
         emptySpace = transform.parent.GetChild(8).gameObject;
+        correctPosition = transform.position;
     }
 
     // Switch places with [EmptySpace] gameobject if it is close enough
@@ -16,9 +19,31 @@ public class SlidingBlock : MonoBehaviour
     {
         if (Vector3.Distance(emptySpace.transform.position, transform.position) < 0.3)
         {
-            Vector3 lastEmptySpacePosition = emptySpace.transform.position;
-            emptySpace.transform.position = transform.position;
-            transform.position = lastEmptySpacePosition;
+            Vector3 lastTilePosition = transform.position;
+            StartCoroutine(Slide(emptySpace.transform.position)); // Start a coroutine that moves the block
+            emptySpace.transform.position = lastTilePosition;
         }
+    }
+
+    public bool CheckPlace()
+    {
+        return transform.position == correctPosition;
+    }
+    
+    // Slide block to the empty space
+    IEnumerator Slide(Vector3 finalPos)
+    {
+        while (transform.position != finalPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, finalPos, moveSpeed * Time.deltaTime);
+            yield return 0;
+        }
+
+        // Check for completion
+        if (transform.position == correctPosition)
+        {
+            transform.parent.GetComponent<SlidePuzzleManager>().CheckForCompletion();
+        }
+        yield return null;
     }
 }
