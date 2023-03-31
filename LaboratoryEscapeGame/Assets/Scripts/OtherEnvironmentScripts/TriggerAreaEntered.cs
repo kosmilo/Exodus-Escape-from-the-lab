48005baa851ea02df45bc5608bf91a45e2b02913
kosmilo <115208829+kosmilo@users.noreply.github.com>
2023-03-31 09:34:08 +0300

    Puzzle design

--- LaboratoryEscapeGame/Assets/Scripts/OtherEnvironmentScripts/TriggerAreaEntered.cs
+++ LaboratoryEscapeGame/Assets/Scripts/OtherEnvironmentScripts/TriggerAreaEntered.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class FuseBoxExplosion : MonoBehaviour
+public class TriggerAreaEntered : MonoBehaviour
 {
-    [SerializeField] UnityEvent reachedExplosionArea;
+    [SerializeField] UnityEvent areaEntered;
+    [SerializeField] bool changeObjective;
+    [SerializeField] string newObjective;
 
     private void Start() {
-        reachedExplosionArea.AddListener(UpdateObjs);
+        if (changeObjective) {
+            areaEntered.AddListener(UpdateObjs);
+        }
     }
+
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
-            Debug.Log("Trigger fusebox explosion");
-            reachedExplosionArea.Invoke();
+            Debug.Log("Trigger area entered");
+            areaEntered.Invoke();
             Destroy(gameObject);
         }
     }
 
     private void UpdateObjs() {
         ObjectivesManager objectives = FindObjectOfType<ObjectivesManager>();
-        objectives.UpdateObjective("Find parts to fix the elevator [0/3]");
+        objectives.UpdateObjective(newObjective);
     }
 }
