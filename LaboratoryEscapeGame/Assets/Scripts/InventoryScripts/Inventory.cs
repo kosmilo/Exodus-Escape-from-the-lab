51812835cc4ffb4df1e77e3de0eccb988696d59f
Milo Koskela <115208829+kosmilo@users.noreply.github.com>
2023-04-06 14:17:22 +0300

    Update Inventory.cs

--- LaboratoryEscapeGame/Assets/Scripts/InventoryScripts/Inventory.cs
+++ LaboratoryEscapeGame/Assets/Scripts/InventoryScripts/Inventory.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using static UnityEditor.Progress;
 
 // Inventory in player game object
 public class Inventory : MonoBehaviour
